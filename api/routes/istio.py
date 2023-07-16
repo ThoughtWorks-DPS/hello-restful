19316db36025f025464f807dff35021c377b0bae
Nic Cheneweth <nchenewe@thoughtworks.com>
2023-07-16 15:16:18 -0500

    [nc] assess full envoy reponse
    Signed-off-by: Nic Cheneweth <nchenewe@thoughtworks.com>

--- api/routes/istio.py
+++ api/routes/istio.py
@@ -26,7 +26,7 @@ async def get_envoy_metadata(response: Response):
         response.status_code = status.HTTP_200_OK
         server_response = requests.get(settings.server_info_url, timeout=10)
         json_response = json.loads(server_response.content)
-        locality = json_response['node']['locality']
+        #locality = json_response['node']['locality']
         #return {"locality": locality}
         return json_response
     except requests.exceptions.ConnectionError:
