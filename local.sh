d36e474c7a075e897a6f27158bee801123057fd4
Nic Cheneweth <nchenewe@thoughtworks.com>
2025-02-13 21:46:43 -0600

    [nc] release testing
    Signed-off-by: Nic Cheneweth <nchenewe@thoughtworks.com>

--- local.sh
+++ local.sh
@@ -11,7 +11,7 @@ case $MODE in
       --namespace "demo-$ENV" \
       --values charts/hello-restful/values.yaml \
       --values "charts/hello-restful/values-$ENV.yaml" \
-      --set image.tag=dev.ce1e002
+      --set image.tag=0.4.1
     ;;
 
   "t")
