69633ed54658232d4817299250b3968333142055
Nic Cheneweth <nchenewe@thoughtworks.com>
2025-02-14 17:12:49 -0600

    [nc] refactor
    Signed-off-by: Nic Cheneweth <nchenewe@thoughtworks.com>

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -6,7 +6,7 @@ tag=$2
 
 echo "deploy hello-restful:$tag to demo-$env"
 helm upgrade hello-restful charts/hello-restful \
-      --install \
+      --install --atomic --timeout 120s \
       --namespace "demo-$env" \
       --values "charts/hello-restful/values.yaml" \
       --values "charts/hello-restful/values-$env.yaml" \
