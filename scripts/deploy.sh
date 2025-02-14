1b0b795913404862f8227d8c003fec5edc2d1695
Nic Cheneweth <nchenewe@thoughtworks.com>
2025-02-14 13:35:22 -0600

    [nc] refactor
    Signed-off-by: Nic Cheneweth <nchenewe@thoughtworks.com>

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,13 +1,13 @@
 #!/usr/bin/env bash
 set -eo pipefail
 
-environment="$1"
-tag="$2"
+export env="$1"
+export tag="$2"
 
-echo "deploy hello-restful:$tag to demo-$environment"
+echo "deploy hello-restful:$tag to demo-$env"
 helm upgrade hello-restful charts/hello-restful \
       --install --atomic --timeout 60s \
-      --namespace "demo-$environment" \
+      --namespace "demo-$env" \
       --values "charts/hello-restful/values.yaml" \
-      --values "charts/hello-restful/values-$environment.yaml" \
+      --values "charts/hello-restful/values-$env.yaml" \
       --set image.tag="$tag"
